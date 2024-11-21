a202a2121c8188e4cb9135f68789e8fe812afa72
Abizar Ar Rifa'i <abizararrifai@gmail.com>
2024-11-21 23:12:13 +0700

    membuat component navlink untuk component navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,16 +9,8 @@
                     <div class="hidden md:block">
                         <div class="flex items-baseline ml-10 space-x-4">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <a href="dashboard" class="px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-md"
-                                aria-current="page">Dashboard</a>
-                            <a href="team"
-                                class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Team</a>
-                            <a href="#"
-                                class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Projects</a>
-                            <a href="#"
-                                class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Calendar</a>
-                            <a href="#"
-                                class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Reports</a>
+                            <x-navlink href='/dashboard' :active="request()->is('dashboard')">Dashboard</x-navlink>
+                            <x-navlink href='/team' :active="request()->is('team')">Team</x-navlink>
                         </div>
                     </div>
                 </div>
@@ -104,16 +96,8 @@
         <div x-show='isOpen' class="md:hidden" id="mobile-menu">
             <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="dashboard" class="block px-3 py-2 text-base font-medium text-white bg-gray-900 rounded-md"
-                    aria-current="page">Dashboard</a>
-                <a href="team"
-                    class="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Team</a>
-                <a href="#"
-                    class="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Projects</a>
-                <a href="#"
-                    class="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Calendar</a>
-                <a href="#"
-                    class="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Reports</a>
+                <x-navlink href='/dashboard' :active="request()->is('dashboard')">Dashboard</x-navlink>
+                <x-navlink href='/team' :active="request()->is('team')">Team</x-navlink>
             </div>
             <div class="pt-4 pb-3 border-t border-gray-700">
                 <div class="flex items-center px-5">
