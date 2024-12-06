4fee8a5873b926d4dd79e9d477d12644106820f1
Abizar Ar Rifa'i <abizararrifai@gmail.com>
2024-12-06 23:21:46 +0700

    menampilkan data pada halaman web

--- app/Http/Controllers/ClubController.php
+++ app/Http/Controllers/ClubController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Club;
 use Illuminate\Http\Request;
 
 class ClubController extends Controller
@@ -11,7 +12,8 @@ class ClubController extends Controller
      */
     public function index()
     {
-        return view('club', ['title' => 'Clubs']);
+        $clubs = Club::all();
+        return view('club', ['title' => 'Clubs'], compact('clubs'));
     }
 
     /**
--- resources/views/club.blade.php
+++ resources/views/club.blade.php
@@ -2,6 +2,23 @@
     <x-navbar></x-navbar>
     <x-header>{{ $title }}</x-header>
     <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <h1>content</h1>
+        <table class="border border-separate border-slate-500">
+            <thead>
+                <tr>
+                    <th class="border border-slate-600">Nama Club</th>
+                    <th class="border border-slate-600">City</th>
+                    <th class="border border-slate-600">Stadium</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($clubs as $club)
+                    <tr>
+                        <td class="px-2 border border-slate-700">{{ $club->name }}</td>
+                        <td class="px-2 border border-slate-700">{{ $club->city }}</td>
+                        <td class="px-2 border border-slate-700">{{ $club->stadium }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </x-layout>
